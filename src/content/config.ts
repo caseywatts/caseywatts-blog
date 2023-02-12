f032057fbed68ef5e63ddc80f0ed5cc30af63b05
Casey Watts <casey.s.watts@gmail.com>
2023-02-12 15:55:59 -0500

    extract blog layout from general layout; use slot for adding head content; center blog posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,6 +4,7 @@ const blog = defineCollection({
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
+		tagline: z.string(),
 		description: z.string(),
 		// Transform string to Date object
 		pubDate: z
