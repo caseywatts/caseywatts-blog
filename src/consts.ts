c7ef7c6b09d594fc5970432ac8ae12fb88fdf8be
Casey Watts <casey.s.watts@gmail.com>
2023-02-13 01:04:35 -0500

    add rss feed; import my-story

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Hi, I'm Casey!";
+export const SITE_DESCRIPTION = 'I love to help people and groups become more happy and effective 🌈';
