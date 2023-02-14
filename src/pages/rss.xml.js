5cd6278d1086d9c338e51b512dfc91fab3b6615e
Casey Watts <casey.s.watts@gmail.com>
2023-02-14 16:16:02 -0500

    auto-generate an rss description from title and tagline if there is none.

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,13 +4,18 @@ import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function get(context) {
   const posts = await getCollection("blog");
+
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: posts.map((post) => ({
-      ...post.data,
-      link: `/blog/${post.slug}/`,
-    })),
+    items: posts.map((post) => {
+      const description = post.description || `${post.emoji} ${post.title} ${post.tagline}`;
+      return {
+        ...post.data,
+        description,
+        link: `/blog/${post.slug}/`,
+      };
+    }),
   });
 }
