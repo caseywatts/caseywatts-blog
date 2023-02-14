a489116fda3347953e9bfeea978b90047d68159d
Casey Watts <casey.s.watts@gmail.com>
2023-02-13 23:31:57 -0500

    adjusting css; centering embedded tweet

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            h1: {
+              fontWeight: 200,
+            },
+            h2: {
+              fontWeight: 400,
+            },
+            h3: {
+              fontWeight: 500,
+            },
+          },
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
