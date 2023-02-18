6bcae7463f6d0a242bc90fc7f580dd9649741902
Casey Watts <casey.s.watts@gmail.com>
2023-02-18 02:06:35 -0500

    only show webmentions if there are any

--- src/components/Webmentions.svelte
+++ src/components/Webmentions.svelte
@@ -14,10 +14,12 @@
   // const mentionCount = fetch(`https://webmention.io/api/count?target=${url}`).then((response) => response.json());
 </script>
 
-{#each mentions as mention}
+{#if mentions.length}
   <div class="text-lg mt-6 mb-2">Liked By</div>
-  <a href={mention.url} class="bg-slate-200 hover:bg-slate-100 border border-slate-700 hover:border-slate-600 p-2 my-1 flex align-middle justify-center items-center space-x-2">
-    <img class="w-8 rounded-full" src={mention.author.photo} alt="profile photo of {mention.author.name}" />
-    <span>{mention.author.name}</span>
-  </a>
-{/each}
+  {#each mentions as mention}
+    <a href={mention.url} class="bg-slate-200 hover:bg-slate-100 border border-slate-700 hover:border-slate-600 p-2 my-1 flex align-middle justify-center items-center space-x-2">
+      <img class="w-8 rounded-full" src={mention.author.photo} alt="profile photo of {mention.author.name}" />
+      <span>{mention.author.name}</span>
+    </a>
+  {/each}
+{/if}
