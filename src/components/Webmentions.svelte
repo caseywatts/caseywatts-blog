3178473f2a416161e483014c26de6b3f56430faf
Casey Watts <casey.s.watts@gmail.com>
2023-02-20 18:37:38 -0500

    formatting for webmentions; only fetch likes for now

--- src/components/Webmentions.svelte
+++ src/components/Webmentions.svelte
@@ -1,25 +1,28 @@
 <script>
   export const url = "https://www.caseywatts.com/blog/making-event-descriptions-welcoming/"; // UDATE
 
-  $: mentions = [];
-  fetch(`https://webmention.io/api/mentions.jf2?target=${url}`)
+  $: likes = [];
+  fetch(`https://webmention.io/api/mentions.jf2?target=${url}&wm-property=like-of`)
     .then((response) => {
       return response.json();
     })
     .then((responseJson) => {
       console.log(responseJson.children);
-      mentions = responseJson.children;
+      likes = responseJson.children;
     });
 
   // const mentionCount = fetch(`https://webmention.io/api/count?target=${url}`).then((response) => response.json());
 </script>
 
-{#if mentions.length}
-  <div class="text-lg mt-6 mb-2">Liked By</div>
-  {#each mentions as mention}
-    <a href={mention.url} class="bg-slate-200 hover:bg-slate-100 border border-slate-700 hover:border-slate-600 p-2 my-1 flex align-middle justify-center items-center space-x-2">
-      <img class="w-8 rounded-full" src={mention.author.photo} alt="profile photo of {mention.author.name}" />
-      <span>{mention.author.name}</span>
-    </a>
+{#if likes.length}
+  <div class="text-lg mt-6 mb-2">Twitter & Mastodon Likes</div>
+  {#each likes as mention}
+    <div class="flex flex-row gap-4 justify-center">
+      <a href={mention.url} class="bg-slate-200 hover:bg-slate-100 border border-slate-700 hover:border-slate-600 p-2 my-1 flex align-middle justify-center items-center gap-3 rounded-full">
+        <span>❤️ by</span>
+        <img class="w-8 rounded-full" src={mention.author.photo} alt="profile photo of {mention.author.name}" />
+        <span>{mention.author.name}</span>
+      </a>
+    </div>
   {/each}
 {/if}
