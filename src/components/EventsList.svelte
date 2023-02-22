91d5fddf661d577bf789a1f71a4fc8a5c35e451d
Casey Watts <casey.s.watts@gmail.com>
2023-02-22 17:55:25 -0500

    add events page (wip)

---
+++ src/components/EventsList.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { EVENTS } from "../events.ts";
+  const events = EVENTS.sort((a, b) => b.date.valueOf() - a.date.valueOf());
+</script>
+
+{#each events as event}
+  <div class="p-4 bg-violet-200">
+    <div>
+      {event.emoji}
+      {event.name}
+    </div>
+    {event.location}
+    {event.description}
+    {event.participating}
+    {event.cost}
+    {event.date}
+  </div>
+{/each}
---
+++ src/events.ts
@@ -0,0 +1,75 @@
+export const EVENTS = [
+  {
+    name: "DC Product & Pastries",
+    emoji: "🥐",
+    location: "downtown DC",
+    description: "a social event / discussion for Product Designers + Product Managers",
+    participating: "hosting",
+    cost: "free",
+    date: "2023-02-21"
+  },
+  {
+    name: "Alexa's Birthday Comedy Show",
+    emoji: "🤣",
+    location: "Baltimore (Hampden)",
+    description: "My friend Alexa is celebrating her birthday with standup/improv. My partner Brian is performing with her, too!",
+    participating: "attending",
+    cost: "$10",
+    date: "2023-02-23"
+  },
+  {
+    emoji: "☕️",
+    name: "Baltimore Code and Coffee",
+    location: "Baltimore (Hampden)",
+    description: "a social event for tech people",
+    participating: "hosting",
+    cost: "free",
+    date: "2023-02-25"
+  },
+  {
+    emoji: "🎶",
+    name: "Baltimore Musical Improv Meetup",
+    location: "Downtown Baltimore",
+    description: "free interactive workshop - learn musical improv. no music or improv background required.",
+    participating: "hosting",
+    cost: "free",
+    date: "2023-03-01"
+  },
+  {
+    emoji: "👬",
+    name: "Cultivating a Community-led Community",
+    location: "online workshop (paid)",
+    description: "Casey will be teaching this 2-hour paid interactive workshop, for anyone interested in sustainable community building",
+    participating: "hosting",
+    cost: "$86",
+    date: "2023-03-13"
+  },
+  {
+    emoji: "🎲",
+    name: "Unpub",
+    location: "North of Baltimore",
+    description: "indie board game conference, free for players",
+    participating: "attending",
+    cost: "free",
+    date: "2023-03-15",
+    endDate: "2023-03-20"
+  },
+  {
+    emoji: "🎨",
+    name: "MICA Grassroots Designfest",
+    location: "downtown Baltimore",
+    description: "a hack-day for designers to do social good projects",
+    participating: "attending",
+    cost: "free",
+    date: "2023-03-25"
+  },
+  {
+    emoji: "🥇",
+    name: "Casey's First Improv Show",
+    location: "downtown DC",
+    description: "the capstone performance for the Intro to Improv course Casey is taking",
+    participating: "performing",
+    cost: "free",
+    date: "2023-04-01"
+  },
+]
