07f6c39df186da77a85f01ac09f56e1fdada1703
Casey Watts <casey.s.watts@gmail.com>
2023-02-23 01:28:17 -0500

    extract out BlogPostPreview component

---
+++ src/components/BlogPostPreview.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let post = {};
+  import FormattedDate from "./FormattedDate.svelte";
+</script>
+
+<a href={`/blog/${post.slug}/`} class="panel block bg-blue-100 visited:bg-purple-100 border border-blue-500 visited:border-purple-500 p-2 md:p-6 my-2 md:my-6 mx-auto prose lg:prose-lg drop-shadow-2xl hover:drop-shadow-xl transition ease-in-out duration-500">
+  <div class="flex flex-col md:flex-row justify-between space-x-0 md:space-x-5 space-y-4 md:space-y-0 align-middle">
+    <div class="text-6xl flex items-center align-middle justify-center">
+      <span>{post.data.emoji}</span>
+    </div>
+    <div class="flex-1 flex flex-col align-middle justify-center items-center md:items-start">
+      <div class="text-2xl">{post.data.title}</div>
+      <div class="text-sm text-slate-600">{post.data.tagline}</div>
+    </div>
+    <div class="text-right italic text-slate-400 text-sm flex items-center justify-center">
+      <span>
+        <FormattedDate date={post.data.pubDate} />
+      </span>
+    </div>
+  </div>
+</a>
---
+++ src/components/FormattedDate.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let date;
+</script>
+
+<time class="inline" datetime={date.toISOString()}>
+  {date.toLocaleDateString("en-us", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  })}
+</time>
