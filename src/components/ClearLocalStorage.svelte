3badf66a89f738b7de76325dd8b903343f1a1795
Casey Watts <casey.s.watts@gmail.com>
2023-02-24 18:08:50 -0500

    add note that checkboxes are saved

--- src/components/ClearLocalStorage.svelte
+++ src/components/ClearLocalStorage.svelte
@@ -13,6 +13,7 @@
   }
 </script>
 
-<div>
+<div class="grid md:grid-cols-2 space-y-4 md:space-y-0 mt-4 md: mt-0">
+  <div class="flex align-middle justify-center items-center text-center">checkboxes are saved<br />in your current browser</div>
   <button class="button-link block mx-auto my-4" on:click={clearLocalStorage}>Clear Checkboxes</button>
 </div>
