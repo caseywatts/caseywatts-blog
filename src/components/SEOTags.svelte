6784e9619348571d07d2aba2dc888b129d398681
Casey Watts <casey.s.watts@gmail.com>
2023-02-24 20:34:30 -0500

    make my own SEO Tags component

---
+++ src/components/SEOTags.svelte
@@ -0,0 +1,32 @@
+<script>
+  export let title = "";
+  export let author = "";
+  export let url = "";
+  export let description = "";
+  export let image = "";
+  export let contentType = "";
+  export let twitterCreator = "";
+</script>
+
+<!-- Regular -->
+<title>{title}</title>
+<meta name="title" content={title} />
+<meta name="author" content={author} />
+<meta name="url" content={url} />
+<meta name="description" content={description} />
+<meta name="image" content={image} />
+
+<!-- OpenGraph -->
+<meta property="og:title" content={title} />
+<meta property="og:url" content={url} />
+<meta property="og:description" content={description} />
+<meta property="og:image" content={image} />
+<!-- <meta name="image" property="og:image:alt" content={imageDescription}> -->
+<meta property="og:type" content={contentType} />
+
+<!-- Twitter -->
+<meta name="twitter:title" content={title} />
+<meta name="twitter:image" content={image} />
+<!-- <meta name="twitter:site" content={url}> -->
+<meta name="twitter:description" content={description} />
+<meta name="twitter:creator" content={twitterCreator} />
