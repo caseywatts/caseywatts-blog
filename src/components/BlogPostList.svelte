3772cde66effc5e4e7cf2fa4e03a632e8b72cd23
Casey Watts <casey.s.watts@gmail.com>
2023-03-05 17:10:21 -0500

    extract BlogPostList component

---
+++ src/components/BlogPostList.svelte
@@ -0,0 +1,20 @@
+<script>
+  import BlogPostPreview from "./BlogPostPreview.svelte";
+  export let allPosts = [];
+
+  const posts = allPosts.filter((post) => !post.data.draft).sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
+</script>
+
+<!-- <script> 
+  const searchParams = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+  });
+  
+  console.log(searchParams.category);
+  </script> -->
+
+<ul>
+  {#each posts as post}
+    <BlogPostPreview {post} />
+  {/each}
+</ul>
