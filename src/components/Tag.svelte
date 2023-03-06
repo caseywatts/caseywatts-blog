69a852781107bbe84a8d78b6fe56098323459232
Casey Watts <casey.s.watts@gmail.com>
2023-03-06 10:08:47 -0500

    more subtle tags

--- src/components/Tag.svelte
+++ src/components/Tag.svelte
@@ -1,3 +1,3 @@
-<span class="absolute top-0 right-0 -translate-y-1/2 md:translate-x-1/2 rotate-3 hover:-rotate-3 transition p-1 mx-0.5 bg-slate-100 border border-violet-300">
+<span class="absolute top-0 right-0 -translate-y-1/2 md:translate-x-1/2 rotate-3 hover:-rotate-3 transition p-1 mx-0.5 bg-violet-100 border-2 border-violet-300 shadow">
   <slot />
 </span>
