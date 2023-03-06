8459ab273b8bc69a0cfe155cbdce741d7056683f
Casey Watts <casey.s.watts@gmail.com>
2023-03-06 11:53:28 -0500

    add position:relative to fix Safari position:absolute bug; remove shadow bc of poor Safari performance

--- src/components/BlogPostPreview.svelte
+++ src/components/BlogPostPreview.svelte
@@ -6,7 +6,7 @@
   let url = post.data.draft ? `/blog/drafts/${post.slug}/` : `/blog/${post.slug}/`;
 </script>
 
-<a href={url} class="link-card group">
+<a href={url} class="link-card group relative">
   {#if post.data.tags}
     <Tag>{post.data.tags}</Tag>
   {/if}
