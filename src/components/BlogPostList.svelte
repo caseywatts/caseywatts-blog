e588414f72235034b0e1030e50a54db6e0b05db5
Casey Watts <casey.s.watts@gmail.com>
2023-03-06 23:18:51 -0500

    transitions/animations when filter is toggled

--- src/components/BlogPostList.svelte
+++ src/components/BlogPostList.svelte
@@ -1,14 +1,27 @@
 <script>
   import BlogPostPreview from "./BlogPostPreview.svelte";
+  import { flip } from "svelte/animate";
+  import { fly } from "svelte/transition";
   export let posts = [];
   export let limit = posts.length;
   export let categoryFilter = "";
 
-  $: filteredPosts = posts.sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()).slice(0, limit);
+  $: filteredPosts = posts
+    .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
+    .slice(0, limit)
+    .filter((post) => {
+      if (categoryFilter == "") {
+        return true;
+      } else {
+        return post.data.tags?.includes(categoryFilter);
+      }
+    });
 </script>
 
 <ul class="block my-10 space-y-10 mb-10">
-  {#each filteredPosts as post}
-    <BlogPostPreview {post} {categoryFilter} />
+  {#each filteredPosts as post (post.data.title)}
+    <div animate:flip={{ duration: 1000 }} transition:fly={{ y: 200, duration: 400 }}>
+      <BlogPostPreview {post} {categoryFilter} />
+    </div>
   {/each}
 </ul>
--- src/components/BlogPostPreview.svelte
+++ src/components/BlogPostPreview.svelte
@@ -1,32 +1,29 @@
 <script>
   export let post = {};
-  export let categoryFilter = "";
   import FormattedDate from "./FormattedDate.svelte";
   import Tag from "./Tag.svelte";
 
   let url = post.data.draft ? `/blog/drafts/${post.slug}/` : `/blog/${post.slug}/`;
 </script>
 
-{#if !categoryFilter || post.data.tags?.includes(categoryFilter)}
-  <div class="panel relative group">
-    <a href={url} class="link-card">
-      <div class="flex flex-col md:flex-row justify-between space-x-0 md:space-x-5 space-y-4 md:space-y-0 align-middle">
-        <div class="text-6xl flex items-center align-middle justify-center">
-          <span>{post.data.emoji}</span>
-        </div>
-        <div class="flex-1 flex flex-col align-middle justify-center items-center md:items-start">
-          <div class="text-2xl">{post.data.title}</div>
-          <div class="text-sm text-slate-600">{post.data.tagline}</div>
-        </div>
-        <div class="text-right italic text-slate-400 text-sm flex items-center justify-center">
-          <span>
-            <FormattedDate date={post.data.pubDate} />
-          </span>
-        </div>
+<div class="panel relative group">
+  <a href={url} class="link-card">
+    <div class="flex flex-col md:flex-row justify-between space-x-0 md:space-x-5 space-y-4 md:space-y-0 align-middle">
+      <div class="text-6xl flex items-center align-middle justify-center">
+        <span>{post.data.emoji}</span>
       </div>
-    </a>
-    {#if post.data.tags}
-      <Tag>{post.data.tags}</Tag>
-    {/if}
-  </div>
-{/if}
+      <div class="flex-1 flex flex-col align-middle justify-center items-center md:items-start">
+        <div class="text-2xl">{post.data.title}</div>
+        <div class="text-sm text-slate-600">{post.data.tagline}</div>
+      </div>
+      <div class="text-right italic text-slate-400 text-sm flex items-center justify-center">
+        <span>
+          <FormattedDate date={post.data.pubDate} />
+        </span>
+      </div>
+    </div>
+  </a>
+  {#if post.data.tags}
+    <Tag>{post.data.tags}</Tag>
+  {/if}
+</div>
