4f50c6c73dbf14b58dcb50b351759e0250b730a7
Casey Watts <casey.s.watts@gmail.com>
2023-03-16 23:53:44 -0400

    add event url to description too

--- src/pages/calendar.ics.js
+++ src/pages/calendar.ics.js
@@ -22,6 +22,7 @@ export async function get(context) {
       allDay: true,
       description: `${event.category} ${event.cost}
 ${event.description}
+${event.url}
 via Casey's Community Calendar`,
       location: event.location,
       url: event.url,
