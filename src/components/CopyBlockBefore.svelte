1ad0d0debce6866a60bea7bc47a564d0dba5d05b
Casey Watts <casey.s.watts@gmail.com>
2023-03-16 22:54:44 -0400

    add bookmarklets page with copy button; make buttons use the pointer cursor

---
+++ src/components/CopyBlockBefore.svelte
@@ -0,0 +1,11 @@
+<script>
+  // must be wrapped in a div with the code block
+  function copyBlockBefore(e) {
+    const code = e.srcElement.closest("astro-island").parentNode.querySelector("code").innerText;
+    navigator.clipboard.writeText(code);
+  }
+</script>
+
+<div class="text-right">
+  <span class="button-link" on:click={copyBlockBefore} on:keydown={copyBlockBefore}>copy</span>
+</div>
