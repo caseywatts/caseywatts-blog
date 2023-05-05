7d59cdb7e4029938b9105361235ab28633aad684
Casey Watts <casey.s.watts@gmail.com>
2023-05-05 10:31:52 -0400

    add copyabletext component; use it on half of the share page

---
+++ src/components/CopyableText.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let text = "ha";
+  $: buttonText = "copy text";
+
+  function copyText() {
+    navigator.clipboard.writeText(text);
+
+    buttonText = "copied text";
+    setTimeout(() => {
+      buttonText = "copy text";
+    }, 1000);
+  }
+</script>
+
+<pre id="linkedin-content" class="bg-slate-50 p-4">{text}</pre>
+<div class="text-right m-2">
+  <button class="button-link" on:click={copyText}>{buttonText}</button>
+</div>
