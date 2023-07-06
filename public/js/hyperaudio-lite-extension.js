e42b67a44541b7697cf746a43595dd5af22c8544
Casey Watts <casey.s.watts@gmail.com>
2023-07-05 21:47:01 -0400

    install hyperaudio-lite; got it working with one episode

---
+++ public/js/hyperaudio-lite-extension.js
@@ -0,0 +1,76 @@
+/*! (C) The Hyperaudio Project. MIT @license: en.wikipedia.org/wiki/MIT_License. */
+/*! Version 2.1.1b */
+
+'use strict';
+// Example wrapper for hyperaudio-lite with search and playbackRate included
+
+let searchForm = document.getElementById('searchForm');
+
+if (searchForm) {
+  searchForm.addEventListener('submit', function(event){
+    searchPhrase(document.getElementById('search').value);
+    event.preventDefault();
+  }, false);
+}
+
+
+let searchPhrase = function (phrase) {
+	
+  let htmlWords = document.querySelectorAll('[data-m]');
+  let htmlWordsLen = htmlWords.length;
+	
+  let phraseWords = phrase.split(" ");
+  let phraseWordsLen = phraseWords.length;
+  let matchedTimes = [];
+
+  // clear matched times
+  let searchMatched = document.querySelectorAll('.search-match');
+
+  searchMatched.forEach((match) => {
+    match.classList.remove('search-match');
+  });
+
+  for (let i = 0; i < htmlWordsLen; i++) {
+    let numWordsMatched = 0;
+    let potentiallyMatched = [];
+
+    for (let j = 0; j < phraseWordsLen; j++) {
+      let wordIndex = i + numWordsMatched;
+
+      if (wordIndex >= htmlWordsLen) {
+        break;
+      }
+
+      // regex removes punctuation - NB for htmlWords case we also remove the space
+
+      if (phraseWords[j].toLowerCase() == htmlWords[wordIndex].innerHTML.toLowerCase().replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()\? ]/g,"")) {
+        potentiallyMatched.push(htmlWords[wordIndex].getAttribute('data-m'));
+        numWordsMatched++;
+      } else {
+        break;
+      }
+
+      // if the num of words matched equal the search phrase we have a winner!
+      if (numWordsMatched >= phraseWordsLen) {
+        matchedTimes = matchedTimes.concat(potentiallyMatched);
+      }
+    }
+  }
+
+  // display
+  matchedTimes.forEach(matchedTime => {
+    document.querySelectorAll("[data-m='"+matchedTime+"']")[0].classList.add("search-match");
+  });
+}
+
+window.onload = function() {
+	const playbackRateCtrl = document.getElementById('pbr');
+	const currentPlaybackRate = document.getElementById('currentPbr');
+
+  if (playbackRateCtrl !== null) {
+    playbackRateCtrl.addEventListener('input', function(){
+      currentPlaybackRate.innerHTML = playbackRateCtrl.value;
+      hyperplayer.playbackRate = playbackRateCtrl.value;
+    },false);
+  }
+}
--- src/components/AudioWidget.svelte
+++ src/components/AudioWidget.svelte
@@ -2,7 +2,7 @@
   export let audioUrl = "";
 </script>
 
-<audio controls class="w-full my-4">
+<audio controls class="w-full my-4" id="hyperplayer">
   <source src={audioUrl} type="audio/wave" />
   Your browser does not support the audio tag.
 </audio>
