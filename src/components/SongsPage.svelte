e3e25fca2f5b185f69e5c1eb11848ba7e5efa93f
Casey Watts <casey.s.watts@gmail.com>
2023-09-16 17:22:39 -0400

    add placeholder page /songs

---
+++ src/components/SongsPage.svelte
@@ -0,0 +1,13 @@
+<script>
+  // import { FirebaseApp } from "sveltefire";
+  // import { initializeApp } from "firebase/app";
+  // import { getFirestore } from "firebase/firestore";
+  // import { getAuth } from "firebase/auth";
+
+  // // Initialize Firebase
+  // const app = initializeApp(/* your firebase config */);
+  // const firestore = getFirestore(app);
+  // const auth = getAuth(app);
+</script>
+
+<div class="panel panel-main space-y-4">songs songs</div>
