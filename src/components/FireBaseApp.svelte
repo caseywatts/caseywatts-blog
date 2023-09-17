3625cc999bffebcd6f006be0def0e6cef378e54d
Casey Watts <casey.s.watts@gmail.com>
2023-09-16 21:47:22 -0400

    firestore working

---
+++ src/components/FireBaseApp.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { FirebaseApp } from "sveltefire";
+  import { initializeApp } from "firebase/app";
+  import { getFirestore } from "firebase/firestore";
+  import { getAuth } from "firebase/auth";
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAk7eGXjzq1IvvDSjMtQZ6w99Ih0m2AwEI",
+    authDomain: "chords-db.firebaseapp.com",
+    databaseURL: "https://chords-db-default-rtdb.firebaseio.com",
+    projectId: "chords-db",
+    storageBucket: "chords-db.appspot.com",
+    messagingSenderId: "16982689672",
+    appId: "1:16982689672:web:7472b13c82d71da3d99c3d",
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const firestore = getFirestore(app);
+  const auth = getAuth(app);
+</script>
+
+<FirebaseApp {auth} {firestore}>
+  <slot />
+</FirebaseApp>
--- src/components/SongsPage.svelte
+++ src/components/SongsPage.svelte
@@ -1,13 +1,46 @@
 <script>
-  // import { FirebaseApp } from "sveltefire";
-  // import { initializeApp } from "firebase/app";
-  // import { getFirestore } from "firebase/firestore";
-  // import { getAuth } from "firebase/auth";
-
-  // // Initialize Firebase
-  // const app = initializeApp(/* your firebase config */);
-  // const firestore = getFirestore(app);
-  // const auth = getAuth(app);
+  import { SignedIn, SignedOut, Doc, Collection } from "sveltefire";
+  import { signInAnonymously } from "firebase/auth";
+  import FireBaseApp from "./FireBaseApp.svelte";
+  import Tag from "./Tag.svelte";
 </script>
 
-<div class="panel panel-main space-y-4">songs songs</div>
+<FireBaseApp>
+  <!-- <Doc ref="Songs/id" let:data>
+    <h2>{data.title}</h2>
+    <p>{data.content}</p>
+  </Doc> -->
+
+  <h2 class="panel panel-main text-2xl text-center">Songs</h2>
+
+  <Collection ref="Songs" let:data={songs}>
+    <div class="columns-4 break-inside-avoid">
+      <div class=" grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+        {#each songs as song}
+          {#if song.Genre == "90s"}
+            <div class="p-1 m-0">
+              <div class="panel panel-main relative group text-center m-3 break-inside-avoid">
+                <div class="text-lg">
+                  <div class="font-semibold">{song.Title}</div>
+                  <div class="">{song.Artist}</div>
+                </div>
+                <div class="mt-2">
+                  {#if song.Lyrics}
+                    <a href={song.Lyrics} class="inline-block m-2 link">Lyrics</a>
+                  {/if}
+                  {#if song.URL}
+                    <a href={song.URL} class="inline-block m-2 link">Uke Chords</a>
+                  {/if}
+                  {#if song.Typeset_ChordsLyrics}
+                    <a href={song.Typeset_ChordsLyrics} class="inline-block m-2 link">Typeset Chords + Lyrics</a>
+                  {/if}
+                </div>
+                <Tag>{song.Genre}</Tag>
+              </div>
+            </div>
+          {/if}
+        {/each}
+      </div>
+    </div>
+  </Collection>
+</FireBaseApp>
