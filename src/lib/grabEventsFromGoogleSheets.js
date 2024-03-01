066b79ee36368fcb3809a5fa071a1b98399c98cc
Casey Watts <casey.s.watts@gmail.com>
2024-03-01 13:10:06 -0500

    add script to pull csv from google sheets

---
+++ src/lib/grabEventsFromGoogleSheets.js
@@ -0,0 +1,11 @@
+import Papa from "papaparse";
+import fs from "fs";
+
+const googleSheetsURL = "https://docs.google.com/spreadsheets/d/1vvoOtlCDxfh0XPxnsSEPGLut-ommJ5vyuMdk9dCDvi4/export?format=csv";
+
+fetch(googleSheetsURL).then((response) => {
+  response.text().then((eventscsv) => {
+    const eventsjson = Papa.parse(eventscsv, { header: true });
+    fs.writeFileSync("src/events.json", JSON.stringify(eventsjson.data));
+  });
+});
