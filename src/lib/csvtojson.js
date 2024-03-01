96b761ed3878eaf743369a6ab5242fec507a5b54
Casey Watts <casey.s.watts@gmail.com>
2024-03-01 12:45:18 -0500

    use papaparse to convert csv to json, so I can manage my events in csv now

---
+++ src/lib/csvtojson.js
@@ -0,0 +1,6 @@
+import Papa from "papaparse";
+import fs from "fs";
+
+const eventscsv = fs.readFileSync("src/events.csv", { encoding: "utf-8" });
+const eventsjson = Papa.parse(eventscsv, { header: true });
+fs.writeFileSync("src/events.json", JSON.stringify(eventsjson.data));
---
+++ src/lib/jsontocsv.js
@@ -0,0 +1,7 @@
+import EVENTS from "../events.json" assert { type: "json" };
+import Papa from "papaparse";
+import fs from "fs";
+
+const csv = Papa.unparse(EVENTS);
+console.log(csv);
+fs.writeFileSync("events.csv", csv);
