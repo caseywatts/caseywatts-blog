8cf369249af341881d3379cf049132e6b22430aa
Casey Watts <casey.s.watts@gmail.com>
2024-05-05 16:22:09 -0400

    recipes index is working!

--- src/components/RecipesIndex.svelte
+++ src/components/RecipesIndex.svelte
@@ -1,30 +1,15 @@
 <script>
   // import BlogPostList from "./BlogPostList.svelte";
   export let recipes;
-  // let categoryFilter = "";
-  // function updateCategory(category) {
-  //   return function () {
-  //     if (category) {
-  //       categoryFilter = category;
-  //       const path = `/blog?category=${category}`;
-  //       window.history.replaceState({}, {}, path);
-  //     } else {
-  //       categoryFilter = "";
-  //       window.history.replaceState({}, {}, "/blog");
-  //     }
-  //   };
-  // }
-
-  // import { onMount } from "svelte";
-  // onMount(() => {
-  //   const searchParams = new Proxy(new URLSearchParams(window.location.search), {
-  //     get: (searchParams, prop) => searchParams.get(prop),
-  //   });
-  //   categoryFilter = searchParams.category || "";
-  // });
+  import RecipePreview from "./RecipePreview.svelte";
 </script>
 
+<div class="panel panel-main space-y-6">
+  <h2 class="text-3xl text-center">Recipes</h2>
+</div>
+
 <section>
-  {recipes}
-  <!-- <BlogPostList {posts} {categoryFilter} /> -->
+  {#each recipes as recipe}
+    <RecipePreview {recipe} />
+  {/each}
 </section>
