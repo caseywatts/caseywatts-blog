4716b4b7938f06997376157b183907ee3758c556
Casey Watts <casey.s.watts@gmail.com>
2024-06-03 15:42:44 -0400

    import feeds via vite raw import

--- src/components/Feeds.svelte
+++ src/components/Feeds.svelte
@@ -1,20 +1,6 @@
 <script>
   import FeedsForOpml from "./FeedsForOPML.svelte";
-  const baltimoreFeeds = `<?xml version="1.0" encoding="UTF-8"?>
-  <opml xmlns:frss="https://freshrss.org/opml" version="2.0">
-    <head>
-      <title>FreshRSS</title>
-      <dateCreated>Fri, 31 May 2024 15:48:27 +0000</dateCreated>
-    </head>
-    <body>
-      <outline text="Baltimore">
-        <outline text="Baltimore Beat" type="rss" xmlUrl="https://baltimorebeat.com/feed/" htmlUrl="https://baltimorebeat.com/" description="Black-led, Black-controlled news"/>
-        <outline text="Baltimore Fishbowl" type="rss" xmlUrl="https://baltimorefishbowl.com/feed/" htmlUrl="https://baltimorefishbowl.com/" description="YOUR WORLD BENEATH THE SURFACE."/>
-        <outline text="Greater Greater Washington" type="rss" xmlUrl="https://ggwash.org/rss" htmlUrl="https://ggwash.org/" description="Main Greater Greater Washington RSS feed."/>
-        <outline text="Homepage - WYPR" type="rss" xmlUrl="https://www.wypr.org/index.rss" htmlUrl="https://www.wypr.org/" description="WYPR is a public radio station serving the Baltimore, Maryland metropolitan area."/>
-      </outline>
-    </body>
-  </opml>`;
+  import friendsFeeds from "../../public/CaseyFriendsFeeds.xml?raw";
 </script>
 
 <div class="panel panel-main mt-24">
@@ -24,6 +10,6 @@
 
 <div class="panel panel-main mt-24">
   <div class="text-center mb-4">
-    <FeedsForOpml myFeeds={baltimoreFeeds} />
+    <FeedsForOpml myFeeds={friendsFeeds} />
   </div>
 </div>
