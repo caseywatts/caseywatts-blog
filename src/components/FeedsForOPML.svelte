cb4de1222b6fa7517586fa807b363ea87a32cd4d
Casey Watts <casey.s.watts@gmail.com>
2024-06-03 15:47:17 -0400

    include title

--- src/components/FeedsForOPML.svelte
+++ src/components/FeedsForOPML.svelte
@@ -2,6 +2,7 @@
   import fxparser from "fast-xml-parser";
   export let myFeeds = "";
   const feedsXML = new fxparser.XMLParser({ ignoreAttributes: false }).parse(myFeeds);
+  const feedCollectionTitle = feedsXML.opml.body.outline["@_text"];
   const feedsWithDetails = feedsXML.opml.body.outline.outline.map((feed) => {
     return {
       name: feed["@_text"],
@@ -12,17 +13,20 @@
   });
 </script>
 
-<div class="grid grid-cols-2">
+<div class="text-2xl">{feedCollectionTitle}</div>
+<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
   {#each feedsWithDetails as feed}
     <div class="card card-body">
-      <div class="text-2xl">
+      <div class="text-xl">
         <a class="link" href={feed.htmlUrl}>
           {feed.name}
         </a>
       </div>
-      <div class="text-base">{feed.description}</div>
+      {#if feed.description}
+        <div class="text-base">{feed.description}</div>
+      {/if}
       <div>
-        <a class="link" href={feed.xmlUrl}>rss feed</a>
+        <a class="link" href={feed.xmlUrl}>rss</a>
       </div>
     </div>
   {/each}
