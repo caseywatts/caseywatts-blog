2d823b10eecbce1cd94ef232a20d256644841bde
Casey Watts <casey.s.watts@gmail.com>
2024-07-08 14:20:17 -0400

    add external links test + opt-out for etsy and linkedin since they're flaky + fix urls that were broken

--- cypress/e2e/index.cy.js
+++ cypress/e2e/index.cy.js
@@ -1,5 +1,23 @@
+const skipRequestValidation = ["https://www.etsy.com/shop/heycaseywattsup/", "https://linkedin.com/in/heycaseywattsup"];
+
 it("titles are correct", () => {
   const page = cy.visit("/");
 
   page.get("title").should("have.text", "Hi, I'm Casey!");
+});
+
+it("footer links all work", () => {
+  const page = cy.visit("/");
+  cy.get("a").each((link) => {
+    const href = link.prop("href");
+    if (href) {
+      if (skipRequestValidation.includes(href)) {
+        return; // skip these
+      } else {
+        cy.request({
+          url: href,
+        });
+      }
+    }
+  });
 });
