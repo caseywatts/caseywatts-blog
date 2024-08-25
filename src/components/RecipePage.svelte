d2775ddb6749d3fcee655b94e1246285340bc8a9
Casey Watts <casey.s.watts@gmail.com>
2024-08-25 10:56:59 -0400

    recipe page styling

--- src/components/RecipePage.svelte
+++ src/components/RecipePage.svelte
@@ -3,17 +3,31 @@
   import Ingredient from "./Ingredient.svelte";
   import Cookware from "./Cookware.svelte";
   import Step from "./Step.svelte";
+
+  import { onMount } from "svelte";
+  import { connectCheckboxesToLocalStorage, clearLocalStorage } from "../lib/localStorageCheckboxes";
+
+  onMount(() => {
+    connectCheckboxesToLocalStorage();
+  });
 </script>
 
 <div class="grid grid-cols-2">
-  <div><h1>{recipe.metadata.name} {recipe.metadata.emoji}</h1></div>
-  <div class="text-right">
+  <div class="mb-4">
+    <h1 class="mb-2">{recipe.metadata.name} {recipe.metadata.emoji}</h1>
+    {#if recipe.metadata.source}
+      <div>
+        Source: {recipe.metadata.source}
+      </div>
+    {/if}
+
     {#if recipe.metadata.sourceurl}
       <div>
         <a href={recipe.metadata.sourceurl}>Source</a>
       </div>
     {/if}
-
+  </div>
+  <div class="bg-purple-200 p-2 rounded-full text-center flex flex-col align-middle justify-center">
     {#if recipe.metadata.totaltime}
       <div>
         Total Time: {recipe.metadata.totaltime}
@@ -25,6 +39,9 @@
         Servings: {recipe.metadata.servings}
       </div>
     {/if}
+    <div>
+      <button class="link inline-block" on:click={clearLocalStorage}>Clear Checkboxes</button>
+    </div>
   </div>
 </div>
 
@@ -34,7 +51,10 @@
       <div class="text-2xl">Ingredients</div>
       <ul>
         {#each recipe.ingredients as ingredient}
-          <li><Ingredient {ingredient} /></li>
+          <li>
+            <input type="checkbox" id={ingredient.name} class="save-to-local-storage" />
+            <label for={ingredient.name}><Ingredient {ingredient} /> </label>
+          </li>
         {/each}
       </ul>
     </div>
